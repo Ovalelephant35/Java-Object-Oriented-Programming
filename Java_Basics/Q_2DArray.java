7bc851e402c7b1f24fe8e91583ca2f3170b7d7c0
Oval_Elephant35 <principalquantum30@gmail.com>
2023-07-06 17:59:45 +0530

    Java basics

---
+++ Java_Basics/Q_2DArray.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Q_2DArray {
+    public static void main(String[] args){
+        Scanner scan = new Scanner (System.in);
+        String [][] cars = new String[2][2];
+            for(int j=0 ; j<2 ; j++){
+                System.out.println("Enter the name of the car :"+(j+1));
+                cars[0][j] = scan.nextLine();
+            }
+            for(int j=0 ; j<2 ; j++){
+                System.out.println("Enter the Milage  of the car : "+cars[0][j]);
+                cars[1][j] = scan.nextLine();
+            }
+            for(int j=0 ; j<2 ; j++){
+                System.out.println("Name of the car and milage of cars are respectively: "+cars[0][j]+ " " +cars[1][j]);
+            }
+            scan.close();
+
+    }
+}
---
+++ Java_Basics/S_Wrapperclass.java
@@ -0,0 +1,10 @@
+public class S_Wrapperclass {
+    public static void main(String[] args){
+        Integer a = 5;
+        if(a.equals(5)){
+            System.out.println("yes");
+        }
+    }
+}
+//used to extract property of Reference data types for primitive data types
+//basically we used string property for integer
