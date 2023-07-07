229fe2869d23010785b1c84c9377de04201686ff
Oval_Elephant35 <principalquantum30@gmail.com>
2023-07-07 09:46:31 +0530

    Java Basics

---
+++ Java_Basics/Y_Printf.java
@@ -0,0 +1,14 @@
+public class Y_Printf {
+    public static void main(String[] args){
+        boolean b = true;
+        int ans = 56;
+        double al = 5.6;
+        System.out.printf("%b" , b);
+        System.out.println();
+        System.out.printf("%d" , ans);
+        System.out.println();
+        System.out.printf("%f" , al);
+        System.out.println();
+
+    }
+}
