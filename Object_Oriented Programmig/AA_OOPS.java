5d64c703559ea8d05a3d5953f44e539b31b261c2
Oval_Elephant35 <principalquantum30@gmail.com>
2023-07-07 14:37:46 +0530

    OOPS

---
+++ Object_Oriented Programmig/AA_OOPS.java
@@ -0,0 +1,19 @@
+public class AA_OOPS {
+    public static void main(String[] args){
+        Car car = new Car();
+        System.out.println(car.name);
+        car.running();
+        car.breaking();    
+    }
+}
+
+class Car{
+    String name = "Toyota" ;
+
+    void running(){
+        System.out.println("Your car is Speeding");
+    }
+    void breaking(){
+        System.out.println("Your car is Stopping");
+    }
+    }
