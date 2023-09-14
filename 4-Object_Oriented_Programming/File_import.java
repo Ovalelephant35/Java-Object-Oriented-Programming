e4246701a020238db730499803e0232ebd56c5de
Anurag Singh <principalquantum30@gmail.com>
2023-09-14 21:27:06 +0530

    OOPS

---
+++ 4-Object_Oriented_Programming/File_import.java
@@ -0,0 +1,9 @@
+//let say we have to use import there are two types implicit and explicit imports of file
+//Also when you import any file they are only imported on the surface but not there subclasses
+//here pattern is contained in the java-->util-->regex file so we have import all of them 
+import java.util.regex.*;
+public class File_import {
+    public static void main(String[] args){
+        Pattern p = Pattern.compile("ab");
+    }
+}
