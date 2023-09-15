5a155aee1ee3dc0833f0e39ffeadfaa8ecb44ad7
Anurag Singh <principalquantum30@gmail.com>
2023-09-15 21:42:29 +0530

    OOPS

---
+++ 4-Object_Oriented_Programming/Abstract_Class.java
@@ -0,0 +1,9 @@
+public abstract class Abstract_Class {
+    public abstract void key();
+    public static void main(){
+        System.out.println("");
+    }
+}
+//something that is incomplete or is not fixed can be set to abstract class 
+//abstract method is one without body and is done with semicolon 
+//if any class contains abstract  method then it should be under abstract class only
