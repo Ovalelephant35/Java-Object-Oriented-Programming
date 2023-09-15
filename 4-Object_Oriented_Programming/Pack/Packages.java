7be2e7c58477c8fd5b113054fd2e4af82ff19a26
Anurag Singh <principalquantum30@gmail.com>
2023-09-15 20:51:55 +0530

    OOPS

---
+++ 4-Object_Oriented_Programming/Pack/Packages.java
@@ -0,0 +1,13 @@
+package Pack;
+public class Packages{
+    void greet(){
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args){
+        
+    }
+}
+// We can make packages out of the similar files 
+// It has to be first statment and we can hve atmost 1 package file
+// to compile it and make a package file we have to have following cmds to compile javac -d . filename
+// to further execute the file we have to have the following cmds java folder.folder.filename
---
+++ 4-Object_Oriented_Programming/Pack/Packages2.java
@@ -0,0 +1,8 @@
+package Pack;
+public class Packages2 {
+    public static void main(String[] args){
+        Packages P = new Packages();
+        P.greet();
+    }
+}
+//we can access the packages as they are public class but compilation can only be done using cmds that were previously used not the og one.
