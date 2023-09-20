ca7b403986d59bc812b9ee124e428485dee0e025
Anurag Singh <principalquantum30@gmail.com>
2023-09-20 19:08:16 +0530

    OOPs

---
+++ 4-Object_Oriented_Programming/Member_Modifier/Member.java
@@ -0,0 +1,11 @@
+package Member_Modifier;
+
+public class Member {
+    void m1(){
+        System.out.println("This is a Public Method");
+    }
+    void m2(){
+        System.out.println("This is a private member method");
+    }
+}
+//still as default class can be accessed within same package 
---
+++ 4-Object_Oriented_Programming/Member_Modifier/Member2.java
@@ -0,0 +1,10 @@
+package Member_Modifier;
+
+public class Member2 {
+    public static void main(String[] args){
+        Member M = new Member();
+        M.m1();
+        // M.m2(); cannot be accessed
+        
+    }
+}
