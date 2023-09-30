4fd84fb0b3b751adb4b94cbd152d4b4ddf58447f
Anurag Singh <principalquantum30@gmail.com>
2023-10-01 00:34:13 +0530

    OOPs

--- 4-Object_Oriented_Programming/Overriding_Variable.java
+++ 4-Object_Oriented_Programming/Overriding_Variable.java
@@ -1,3 +1,17 @@
+//there is nor concept of variable overriding in oops /java
+class Variable{
+    String s = "Anurag";
+}
+class Childvar extends Variable{
+    String s = "Singh";
+}
 public class Overriding_Variable {
-    
+    public static void main(String[] args){
+        Variable g1 = new Variable();
+        System.out.println(g1.s);
+        Childvar gg = new Childvar();
+        System.out.println(gg.s);
+        Variable gf = new Childvar();
+        System.out.println(gf.s);//based on refernce types only 
+    }
 }
