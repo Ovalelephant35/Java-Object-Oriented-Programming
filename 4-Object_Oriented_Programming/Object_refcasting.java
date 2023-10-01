75e8c9d4a31b2a43290ae2391de41e59fdd2ca32
Anurag Singh <principalquantum30@gmail.com>
2023-10-01 11:40:50 +0530

    OOPs

---
+++ 4-Object_Oriented_Programming/Object_refcasting.java
@@ -0,0 +1,23 @@
+class ast{
+    public  void m1(){
+        System.out.println("This is class A");
+    }
+}
+class bst extends ast{
+    public  void m1(){
+        System.out.println("This is class B");
+    }
+}
+class cst extends bst{
+    public  void m1(){
+        System.out.println("This is class C");
+    }
+}
+public class Object_refcasting {
+    public static void main(String[] args){
+        cst c = new cst();
+        c.m1();
+        ((bst) c).m1();//based on refernce type as it refernce type when static keyword is used
+        ((ast)((bst) c)).m1();//based on refernce type as it is refernce type
+    }
+}
