fb4e34a2a8fea6bf16c5eb8f97d2ceb98410935e
Anurag Singh <principalquantum30@gmail.com>
2023-11-15 03:45:28 +0530

    OOPs Practice

--- 5-OOPs-Practice/Basic-Concept/Strings.java
+++ 5-OOPs-Practice/Basic-Concept/Strings.java
@@ -1,3 +1,11 @@
 public class Strings {
-    
+    public static void main(String[] args){
+        String name = new String("Anurag");//though there is no need to create new object and it can be used directly
+        System.out.println("Hello "+name);
+        System.out.println(name.charAt(0));
+        System.out.println(name.concat(" Singh"));//string are immutable that is they cannot be changed for mutable use string buffer
+        //string buffer is thread safe and string builder is not thread safe and rest both of them almost same
+        StringBuffer sb = new StringBuffer(name);
+        System.out.println(sb.capacity());
+    }
 }
