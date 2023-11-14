82d2ed323fcd41980e65428584346601423abd2e
Anurag Singh <principalquantum30@gmail.com>
2023-11-14 13:15:34 +0530

    OOPs

--- 4-Object_Oriented_Programming/Multilevel_Inheritance.java
+++ 4-Object_Oriented_Programming/Multilevel_Inheritance.java
@@ -5,33 +5,33 @@ class A2{
     }
 }
 
-class B2 extends A2{
-    public void m6(){
-        System.out.println("This is child class");
-    }
-    // public void m1(){
-    //     System.out.println("This is overriding parent class using child class");
-    // }
-}
+// class B2 extends A2{
+//     public void m6(){
+//         System.out.println("This is child class");
+//     }
+//     // public void m1(){
+//     //     System.out.println("This is overriding parent class using child class");
+//     // }
+// }
 
-class C2 extends B2{
-    public void m9(){
-        System.out.println("This is grandchild class");
-    }
-}
-public class Multilevel_Inheritance {
-    public static void main(String[] args){
-        A2 a  = new A2();
-        a.m10();
-        B2 b  = new B2();
-        b.m10();
-        C2 c  = new C2();
-        c.m10();
-        c.m6();
-        c.m9();
-    }
-}
-class Demo{
+// class C2 extends B2{
+//     public void m9(){
+//         System.out.println("This is grandchild class");
+//     }
+// }
+// public class Multilevel_Inheritance {
+//     public static void main(String[] args){
+//         A2 a  = new A2();
+//         a.m10();
+//         B2 b  = new B2();
+//         b.m10();
+//         C2 c  = new C2();
+//         c.m10();
+//         c.m6();
+//         c.m9();
+//     }
+// }
+// class Demo{
 
 
-}
+// }
