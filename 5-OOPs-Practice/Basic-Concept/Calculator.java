62ef90a47b57448cabff8928bd99f70c9c143d0c
Anurag Singh <principalquantum30@gmail.com>
2023-11-14 22:37:45 +0530

    OOPs Practice

--- 5-OOPs-Practice/Basic-Concept/Calculator.java
+++ 5-OOPs-Practice/Basic-Concept/Calculator.java
@@ -1,20 +1,23 @@
-class calc{
-    int a=12;
-    int b;
-    calc(int a , int b){
-        this.a = a;
-        this.b = b;
-    }
-    public int add(){
+class calc{//local variable are part of stack
+    public int add(int a , int b){
         return a+b;
     }
-    public int sub(){
+    public int add(int a , int b , int c){
+        return a+b+c;
+    }
+    public double add(double a , int b){
+        return a+b;
+    }
+    public String add(String a , String b){
+        return a+b;
+    }
+    public int sub(int a , int b){
         return a-b;
     }
-    public int mul(){
+    public int mul(int a , int b){
         return a*b;
     }
-    public int div(){
+    public int div(int a , int b){
         return a/b;
     }
 
@@ -22,10 +25,11 @@ class calc{
 
 public class Calculator {
     public static void main(String[] args){
-        calc c1 = new calc(17,18);
-        System.out.println(c1.a);
+        calc c1 = new calc();//created inside heap memory 
+        System.out.println(c1.add(5.6 , 2));//local variable are part of stack
     }
 }
+//there is a link between stack and heap with help of address
 //jdk  ->java development kit contains all of this
-//jvm  ->java virtual machine(where code is executed)
+//jVm  ->java virtual machine(where code is executed)
 //jRe  ->java Runtime environment inside which is jvm
